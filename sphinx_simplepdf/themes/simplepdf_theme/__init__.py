17373973ff9191d0c2616c6eadec54b758038318
Jan Breuer <jan.breuer@jaybee.cz>
2023-11-14 11:31:15 +0100

    Squashed commit of the following:
    commit 4f3d2cebde3bfc757de5f237c618cc365ef70d86
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Mon Nov 13 20:43:16 2023 +0100
    
        Reset version to 1.6.0
    
    commit 94278e946c9696dc79feb5ab75d4c7e824b0a157
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Mon Nov 13 20:16:58 2023 +0100
    
        Cleanup artifacts
    
    commit 50538e690c7209ba02089ac4834a1de5edc1acb9
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Mon Nov 13 20:15:30 2023 +0100
    
        Cleanup code
    
    commit d010690f29b263d0aa1f1bb6bc56b5f23d5bb8ac
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Mon Nov 13 17:14:16 2023 +0100
    
        Move CSS generation to theme
    
    commit 1921ef20f72b778951744ca308029e0c1915f5ea
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Mon Nov 13 07:55:32 2023 +0100
    
        Move SASS css generation to the theme
    
    commit 97d1362370bcbb70886af3b2896b14d93735a2fb
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Tue Nov 7 14:35:04 2023 +0100
    
        Bump version: 1.6.0 -> 1.6.1a1
    
    commit f79caf3d49bc1f9e4b2f637bae39dedb0f82da4d
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Tue Nov 7 14:32:09 2023 +0100
    
        Fix pipeline (from poetry to setuptools+twine)
    
    commit e9d6085528bc564cd68a08c7e2edb365530c0764
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Tue Nov 7 14:13:48 2023 +0100
    
        add ci pipeline
    
    commit dcb16ef913e519eee820d81e77491be1d7392814
    Author: Ad Thiers <a.thiers@atcomputing.nl>
    Date:   Mon Nov 6 17:58:20 2023 +0100
    
        Theme fix - use style sources from theme instead from built-in

--- sphinx_simplepdf/themes/simplepdf_theme/__init__.py
+++ sphinx_simplepdf/themes/simplepdf_theme/__init__.py
@@ -3,8 +3,10 @@ From https://github.com/ryan-roemer/sphinx-bootstrap-theme.
 """
 from os import path
 
+import sass
 
-__version__ = '0.1.0'
+
+__version__ = '0.3.0'
 __version_full__ = __version__
 
 
@@ -14,10 +16,33 @@ def get_html_theme_path():
     return cur_dir
 
 
+def create_custom_css(app):
+
+    def get_config_var(name, default):
+        return app.config.simplepdf_vars.get(name, default)
+
+    def get_theme_var(name, default):
+        return app.config.simplepdf_theme_options.get(name, default)
+
+    here = path.abspath(path.dirname(__file__))
+    scss_folder = path.join(here, "static", "styles", "sources")
+
+    staticdir = path.join(app.builder.outdir, "_static")
+
+    sass.compile(
+        dirname=(scss_folder, staticdir),
+        output_style="nested",
+        custom_functions={
+            sass.SassFunction("config", ("$a", "$b"), get_config_var),
+            sass.SassFunction("theme_option", ("$a", "$b"), get_theme_var),
+        },
+    )
+
+
 # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
 def setup(app):
     app.add_html_theme('simplepdf_theme', path.abspath(path.dirname(__file__)))
-    # app.add_css_file('styles/main.css')
+    app.connect('builder-inited', create_custom_css)
 
     return {
         "parallel_read_safe": True,
